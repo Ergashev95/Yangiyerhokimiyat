700997a2df3203589de51307126a2277085e1e1a
MirjalolUrozboev <mirjalol0406@gmail.com>
2022-11-18 14:56:51 +0500

    finished to rekvizit pages

--- js/scripts.js
+++ js/scripts.js
@@ -81,10 +81,75 @@ $(document).ready(function () {
             });
             a = 1;
         }
-
     });
 
 
 
 
+
 })
+
+// calendar start
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.querySelector('.calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        initialDate: '2022-11-07',
+        selectable: true,
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        },
+        events: [
+            {
+                title: 'All Day Event',
+                start: '2022-11-01'
+            },
+            {
+                title: 'Long Event',
+                start: '2022-11-07',
+                end: '2022-11-10'
+            },
+            {
+                groupId: '999',
+                title: 'Repeating Event',
+                start: '2022-11-09T16:00:00'
+            },
+            {
+                groupId: '999',
+                title: 'Repeating Event',
+                start: '2022-11-16T16:00:00'
+            },
+            {
+                title: 'Conference',
+                start: '2022-11-11',
+                end: '2022-11-13'
+            },
+            {
+                title: 'Meeting',
+                start: '2022-11-12T10:30:00',
+                end: '2022-11-12T12:30:00'
+            },
+            {
+                title: 'Lunch',
+                start: '2022-11-12T12:00:00'
+            },
+            {
+                title: 'Meeting',
+                start: '2022-11-12T14:30:00'
+            },
+            {
+                title: 'Birthday Party',
+                start: '2022-11-18T10:18:00'
+            },
+            {
+                title: 'Click for Google',
+                url: 'http://google.com/',
+                start: '2022-11-18'
+            }
+        ]
+    });
+
+    calendar.render();
+});
